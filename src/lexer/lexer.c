915dbb2510bd613649b9daa9ec41cb26c29989c6
Bastien MIRLICOURTOIS <bmirlico@e3r4p10.clusters.42paris.fr>
2023-05-26 19:53:33 +0200

    modif lexer.c

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -6,7 +6,7 @@
 /*   By: bmirlico <bmirlico@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 14:43:42 by bmirlico          #+#    #+#             */
-/*   Updated: 2023/05/26 16:48:42 by bmirlico         ###   ########.fr       */
+/*   Updated: 2023/05/26 19:35:14 by bmirlico         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,13 +31,18 @@ t_token	*create_token(t_token_type type, char *value)
 
 char	*get_token_value(char *input, char *delimiter)
 {
-	int		i;
-	char	*value;
+	static char	*next_input;
+	char		*value;
+	int			i;
 
 	i = 0;
 	if (!input)
+		input = next_input;
+	if (!input)
 		return (NULL);
-	while (input[i] != '\0' && !is_delimiter(input[i], delimiter))
+	while (input[i] != '\0' && is_delimiter(input[i], delimiter))
+		i++;
+	while (input[i] != '\0' && is_delimiter(input[i], delimiter))
 		i++;
 	if (input[i] == '\0')
 		i--;
@@ -55,8 +60,11 @@ int	is_delimiter(char c, char *delimiter)
 	i = 0;
 	while (delimiter[i] != '\0')
 	{
-		
+		if (c == delimiter[i])
+			return (1);
+		i++;
 	}
+	return (0);
 }
 
 void	ft_strncpy(char *value, char *input, int len)
@@ -72,29 +80,42 @@ void	ft_strncpy(char *value, char *input, int len)
 	value[i] = '\0';
 }
 
-char	*get_token_type(char *input, char *delimiter)
+t_token_type	get_token_type(char *value)
 {
-	int	i;
+	int	len;
 
-	i = 0;
-	if (input[i] == '|')
+	len = ft_strlen(value);
+	if (ft_strncmp(value, "|", len) == 0)
 		return (T_PIPE);
-	else if (input[i] == '<')
+	else if (ft_strncmp(value, "<", len) == 0)
 		return (T_LR);
-	else if (input[i] == '>')
+	else if (ft_strncmp(value, ">", len) == 0)
 		return (T_RR);
-	else if (input[i] == '<<')
+	else if (ft_strncmp(value, "<<", len) == 0)
 		return (T_HEREDOC);
-	else if (input[i] == '>>')
+	else if (ft_strncmp(value, ">>", len) == 0)
 		return (T_APPEND);
-	else if (ft_isalpha((int)input[i]))
+	else
 		return (T_WORD);
 }
 
-t_token	**lexer(char *input)
+t_token	*lexer(char *input)
 {
-	t_token	**list;
+	t_token	*list;
+	char	*token_value;
+	char	*token_type;
+	char	*input_copy;
 	int		i;
 
-	
+	i = 0;
+	input_copy = ft_strdup((const char *)input);
+	list = create_list_token();
+	token_value = get_token_value(input_copy, DELIMITER);
+	while (token_value != NULL)
+	{
+		token_type = get_token_type(token_value);
+		create_token(list, token_type, token_value);
+		token_value = get_token_value(NULL, DELIMITER);
+	}
+	free(input_copy);
 }
